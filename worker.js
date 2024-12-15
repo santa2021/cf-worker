e60b54fb86d5e17c4fa311c6f64cd782c2a75995
santa2021 <76956050+santa2021@users.noreply.github.com>
2024-12-15 23:16:55 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -198,7 +198,7 @@ var cf_worker_vless_default = {
     async fetch(request, env, ctx) {
         let address = "";
         let portWithRandomLog = "";
-        const userID = env.UUID || "00fdxxxd-81xx-xx54-bxx2-b6xxx2aXXXXX"; // ENTER YOUR OWN UUID HERE
+        const userID = env.UUID || "7b6c0ace-f54c-44ca-b127-d3027860d058";
         const isVaildUUID = validate_default(userID);
         const log = (info, event) => {
             console.log(`[${address}:${portWithRandomLog}] ${info}`, event || "");
